59b4c6dc93ef01be0dc1af0439233ea7f5c0f0a4
Tia Theisen <tiatheisen@gmail.com>
2022-09-03 13:32:35 -0500

    declared two variables and made an array of numbers

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,9 +5,14 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
 
+let partsNeeded = 40;
+console.log(partsNeeded);
+
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
